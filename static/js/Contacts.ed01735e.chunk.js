a0009c05d73c8500ff56e64504de910536334c2d
Serhii Stanislav <serhiistanislav@gmail.com>
2021-04-16 22:29:15 +0300

    Updates

---
+++ static/js/Contacts.ed01735e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreactor=this.webpackJsonpreactor||[]).push([[1],{44:function(t,n,c){"use strict";c.r(n),c.d(n,"default",(function(){return s}));var r=c(2);function e(){return Object(r.jsx)("p",{children:"Hi from Contacts"})}function s(){return Object(r.jsxs)(r.Fragment,{children:[Object(r.jsx)("h1",{children:"Contacts"}),Object(r.jsx)(e,{})]})}}}]);
+//# sourceMappingURL=Contacts.ed01735e.chunk.js.map
