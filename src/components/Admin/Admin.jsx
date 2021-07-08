47a88429bf18cf90997bf11d215286a0eaa8ab16
Serhii Stanislav <serhiistanislav@gmail.com>
2021-07-08 12:20:56 +0300

    adds two inputs and checkbox

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,19 +1,113 @@
-import { useStyles, muiStyles } from './AdminStyles';
+import { useState } from 'react';
+import { useStyles, muiBtn, muiForm } from './AdminStyles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
 
 // const { log } = console;
 
 const Admin = () => {
+  const [showForm, setshowForm] = useState(false);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [restApi, setRestApi] = useState(false);
   const s = useStyles();
-  const mui = muiStyles();
+  const mb = muiBtn();
+  const mf = muiForm();
+
+  // Form
+  const toggleForm = () => setshowForm(!showForm);
+
+  // Change Input value
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+
+    name === 'name'
+      ? setName(value)
+      : name === 'description'
+      ? setDescription(value)
+      : console.log('ERROR Input value!');
+  };
+
+  // Checkbox
+  const handleChange = e => {
+    setRestApi(e.target.checked);
+  };
+
+  // Submit
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
 
   return (
     <div className={s.Admin}>
       <h2>Portfolio</h2>
-      <form className={mui.root} noValidate autoComplete="off">
-        <TextField id="outlined-basic" label="Name" variant="outlined" />
-        <TextField id="outlined-basic" label="Description" variant="outlined" />
-      </form>
+
+      <div className={s.formWrap}>
+        {showForm ? (
+          <form
+            onSubmit={handleSubmit}
+            className={mf.root}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              id="outlined-basic"
+              name="name"
+              value={name}
+              onChange={handleInputChange}
+              label="Name"
+              variant="outlined"
+            />
+            <TextField
+              id="outlined-basic"
+              name="description"
+              value={description}
+              onChange={handleInputChange}
+              label="Description"
+              variant="outlined"
+            />
+            <Checkbox
+              checked={restApi}
+              // onC
+              color="primary"
+              onChange={handleChange}
+              inputProps={{ 'aria-label': 'primary checkbox' }}
+            />
+
+            <Button
+              onClick={toggleForm}
+              id="cancelBtn"
+              variant="contained"
+              color="primary"
+            >
+              Cancel
+            </Button>
+            <Button
+              id="addlBtn"
+              type="submit"
+              // className={mf.addBtn}
+              variant="contained"
+              color="primary"
+            >
+              Add
+            </Button>
+          </form>
+        ) : (
+          <Button
+            onClick={toggleForm}
+            className={mb.addProjectBtn}
+            variant="contained"
+            color="primary"
+          >
+            + Add project
+          </Button>
+        )}
+      </div>
+      <ul className={s.requestList}>
+        <li>{name}</li>
+        <li>{description}</li>
+      </ul>
     </div>
   );
 };
--- src/components/Admin/AdminStyles.js
+++ src/components/Admin/AdminStyles.js
@@ -2,17 +2,50 @@ import { createUseStyles } from 'react-jss';
 import { makeStyles } from '@material-ui/core/styles';
 import { refs } from '../refs';
 
+// const cancelButton = document.querySelector('#cancelBtn > span');
+
 // s
 export const useStyles = createUseStyles({
   Admin: {
     '& h2': {
       marginBottom: '30px',
     },
+
+    '& .addProjectBtn': {
+      marginBottom: 30,
+    },
+
+    '& .requestList': {
+      //
+    },
   },
 });
 
-// mui
-export const muiStyles = makeStyles({
+// muiBtn
+export const muiBtn = makeStyles({
+  addProjectBtn: {
+    marginBottom: 30,
+  },
+
+  // '&.MuiButton-containedPrimary': {
+  //   // backgroundColor: 'red',
+  // },
+  // '&.MuiButtonBase-root': {
+  //   '&.MuiButton-root': {
+  //     '&.MuiButton-contained': {
+  //       // '&.makeStyles-root-29': {
+  //       '&.MuiButton-containedPrimary': {
+  //         backgroundColor: 'red',
+  //       },
+  //       // },
+  //     },
+  //   },
+  // },
+  // },
+});
+
+// muiForm
+export const muiForm = makeStyles({
   root: {
     // TextField Wrap (div)
     '& .MuiFormControl-root': {
@@ -68,12 +101,12 @@ export const muiStyles = makeStyles({
         },
       },
     },
-    // MuiOutlinedInput-notchedOutline': {
-    //   borderColor: 'red',
 
-    //   '&:hover': {
-    //     borderColor: 'green',
-    //   },
-    // },
+    // Cancel Btn
+    '& .MuiButton-root': {
+      '&#cancelBtn': {
+        marginRight: 20,
+      },
+    },
   },
 });
